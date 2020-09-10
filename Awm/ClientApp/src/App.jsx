e8b8ab40f18e65da6c3267c690aa7b9f3a336663
Anton-Polkanov <anton.polkanov@prooftec.com>
2020-09-11 01:38:42 +1000

    initial

---
+++ Awm/ClientApp/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Dashboard } from './components/Dashboard';
+import { Employees } from './components/Employees';
+import { Aircrafts } from './components/Aircrafts';
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Dashboard} />
+        <Route path='/aircrafts' component={Aircrafts} />
+        <Route path='/employees' component={Employees} />
+      </Layout>
+    );
+  }
+}
