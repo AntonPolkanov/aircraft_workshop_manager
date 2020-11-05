02d9a4ac7fb54a9a89c783da25575c4602632a9e
AntonPolkanov <polkanov_anton@mail.ru>
2020-11-06 02:26:42 +1100

    fetch Employees

--- Awm/ClientApp/src/components/Employees.js
+++ Awm/ClientApp/src/components/Employees.js
@@ -1,86 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import MaterialTable from 'material-table';
+import DeferredSpinner from "./DeferredSpinner";
+import axios from "axios";
 
-export default function Employees() {
-  const [state, setState] = React.useState({
-    columns: [
-      {title: 'Name', field: 'name'},
-      {title: 'Surname', field: 'surname'},
-      {title: 'Birth Year', field: 'birthYear', type: 'numeric'},
-      {
-        title: 'Birth Country',
-        field: 'birthcountry',
-        lookup: {34: 'Australia', 63: 'Russia', 30: 'Nepal'},
-      },
-    ],
-    data: [
-      {name: 'Anton', surname: 'Polkanov', birthYear: 2020, birthcountry: 63},
-      {
-        name: 'Jason',
-        surname: 'Phua',
-        birthYear: 2019,
-        birthcountry: 34,
-      },
-      {
-        name: 'Narayan',
-        surname: 'Pudasaini',
-        birthYear: 1999,
-        birthcountry: 30
-      }
-    ],
-  });
+export default class Employees extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeesData: [],
+      loading: true
+    }
+  }
+  
+  async componentDidMount() {
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/employees`);
+    const data = await response.data;
+    this.setState({
+      employeesData: data,
+      loading: false
+    })
+  }
+  
+  renderTable(data) {
+    return (
+      <MaterialTable
+        title="Employees"
+        columns={[
+          {title: 'First Name', field: 'firstName'},
+          {title: 'Last Name', field: 'lastName'},
+          {title: 'Position', field: 'position'},
+        ]}
+        data={data}
+      />
+    )
+  }
 
-  return (
-    <MaterialTable
-      title="Employee list"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                var d = new FormData();
-                d.append("lastName", JSON.stringify(data));
-                fetch('employeemanagement/add',
-                  {
-                    method: "POST",
-                    body: d
-                  })
-                  .then(response => response.text())
-                  .then(data => data)
-                return {...prevState, data};
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return {...prevState, data};
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return {...prevState, data};
-              });
-            }, 600);
-          }),
-      }}
-    />
-  );
+  render() {
+    return (
+      <>
+        {this.state.loading
+          ? <DeferredSpinner delay={250}/>
+          : this.renderTable(this.state.employeesData)
+        }
+      </>
+    )  
+  }
 }
